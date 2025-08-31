53d557c10dc51eacec8dd11a531b8a83e1ded820
MuazzezA <ayd.muazzez@gmail.com>
2025-09-01 00:29:44 +0300

    feat: video time and file name text

--- app/src/main/java/com/muazzeza/videostabilizer/MainActivity.kt
+++ app/src/main/java/com/muazzeza/videostabilizer/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.muazzeza.videostabilizer
 
+import android.media.MediaMetadataRetriever
 import android.net.Uri
 import android.os.Bundle
+import android.provider.OpenableColumns
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
@@ -34,12 +36,41 @@ class MainActivity : ComponentActivity() {
 fun VideoPickerScreen() {
     val context = LocalContext.current
     var selectedVideoUri by remember { mutableStateOf<Uri?>(null) }
+    var videoName by remember { mutableStateOf<String?>(null) }
+    var videoDuration by remember { mutableStateOf<String?>(null) }
 
     // Galeri seçici
-    val videoPickerLauncher = rememberLauncherForActivityResult (
+    val videoPickerLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.GetContent()
     ) { uri: Uri? ->
-        selectedVideoUri = uri
+        if (uri != null) {
+            selectedVideoUri = uri
+
+            // Dosya adı
+            val cursor = context.contentResolver.query(uri, null, null, null, null)
+            cursor?.use {
+                if (it.moveToFirst()) {
+                    val nameIndex = it.getColumnIndexOrThrow(OpenableColumns.DISPLAY_NAME)
+                    videoName = it.getString(nameIndex)
+                }
+            }
+
+            // tüm video süresi hesaplama
+            val retriever = MediaMetadataRetriever()
+            retriever.setDataSource(context, uri)
+            val durationStr = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)
+            val durationMs = durationStr?.toLongOrNull() ?: 0L
+            retriever.release()
+
+            val seconds = (durationMs / 1000) % 60
+            val minutes = (durationMs / (1000 * 60)) % 60
+            val hours = (durationMs / (1000 * 60 * 60))
+
+            videoDuration = if (hours > 0)
+                String.format("%d:%02d:%02d", hours, minutes, seconds)
+            else
+                String.format("%02d:%02d", minutes, seconds)
+        }
     }
 
     Column(
@@ -54,7 +85,8 @@ fun VideoPickerScreen() {
         Spacer(modifier = Modifier.height(16.dp))
 
         if (selectedVideoUri != null) {
-            Text(text = "Video Seçildi: ${selectedVideoUri.toString()}")
+            Text(text = "Dosya: ${videoName ?: "Bilinmiyor"}")
+            Text(text = "Süre: ${videoDuration ?: "Bilinmiyor"}")
 
             Spacer(modifier = Modifier.height(16.dp))
 
@@ -82,3 +114,4 @@ fun VideoPickerScreen() {
         }
     }
 }
+
