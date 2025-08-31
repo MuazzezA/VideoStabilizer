7fdfbc495d0cb95980e12a16573eee0a579ab2b4
MuazzezA <ayd.muazzez@gmail.com>
2025-09-01 00:18:00 +0300

    Initial commit

---
+++ app/src/main/java/com/muazzeza/videostabilizer/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.muazzeza.videostabilizer
+
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
+import com.muazzeza.videostabilizer.ui.theme.VideoStabilizerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            VideoStabilizerTheme {
+                VideoPickerScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun VideoPickerScreen() {
+    val context = LocalContext.current
+    var selectedVideoUri by remember { mutableStateOf<Uri?>(null) }
+
+    // Galeri seçici
+    val videoPickerLauncher = rememberLauncherForActivityResult (
+        contract = ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        selectedVideoUri = uri
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Button(onClick = { videoPickerLauncher.launch("video/*") }) {
+            Text(text = "Galeriden Video Seç")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        if (selectedVideoUri != null) {
+            Text(text = "Video Seçildi: ${selectedVideoUri.toString()}")
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // ExoPlayer ile video oynatma
+            val exoPlayer = remember(selectedVideoUri) {
+                ExoPlayer.Builder(context).build().apply {
+                    val mediaItem = MediaItem.fromUri(selectedVideoUri!!)
+                    setMediaItem(mediaItem)
+                    prepare()
+                    playWhenReady = true
+                }
+            }
+
+            DisposableEffect(
+                AndroidView(factory = {
+                    PlayerView(it).apply {
+                        player = exoPlayer
+                    }
+                }, modifier = Modifier.fillMaxWidth().height(250.dp))
+            ) {
+                onDispose {
+                    exoPlayer.release()
+                }
+            }
+        }
+    }
+}
